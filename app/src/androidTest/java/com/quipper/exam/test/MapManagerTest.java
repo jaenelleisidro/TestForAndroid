aa39b50cbc9f79ebfbdae414e09f7e9829a06dfb
Jaenelle Isidro <jaenelleisidro@yahoo.com>
2015-05-20 08:59:51 +0800

    added more test cases

--- app/src/androidTest/java/com/quipper/exam/test/MapManagerTest.java
+++ app/src/androidTest/java/com/quipper/exam/test/MapManagerTest.java
@@ -9,6 +9,7 @@ import com.quipper.exam.test.other.Constant;
 import junit.framework.Assert;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by jaenelleisidro on 5/20/15.
@@ -16,6 +17,7 @@ import java.util.Date;
 public class MapManagerTest extends AndroidTestCase {
     MapManager mapManager;
     long timefrozen=1432069298136l;
+    Date expectedDate;
     @Override
     protected void setUp() throws Exception {
         super.setUp();
@@ -25,13 +27,35 @@ public class MapManagerTest extends AndroidTestCase {
                 return timefrozen;
             }
         };
+        expectedDate=new Date(timefrozen-(Constant.MINUTESDELAYOFMAPAPI *Constant.ONEMINUTEINMILLISECONDS));
     }
 
-    public void testGenerateLatestMap() throws Throwable {
-        Map map=mapManager.generateLatestMap();
-        Date expectedDate=new Date(timefrozen-(Constant.MINUTESDELAYOFMAPAPI *Constant.ONEMINUTEINMILLISECONDS));
+    private void testIfLatestMap(Map map){
         Assert.assertTrue(map.date.equals(expectedDate));
         Assert.assertTrue(map.description.equals("2015-05-19 14:00"));
         Assert.assertTrue(map.imageUrl.equals("http://www.jma.go.jp/jp/gms/imgs/5/infrared/1/201505200500-00.png"));
     }
+
+    public void testGenerateMapForMinutesAgo() throws Throwable {
+        Map map=mapManager.generateMapForMinutesAgo(Constant.MINUTESDELAYOFMAPAPI);
+        testIfLatestMap(map);
+    }
+
+    public void testGenerateLatestMap() throws Throwable {
+        Map map=mapManager.generateLatestMap();
+        testIfLatestMap(map);
+    }
+
+
+    public void testGenerateLatestMaps() throws Throwable {
+        List<Map> maps=mapManager.generateLatestMaps();
+        testIfLatestMap(maps.get(0));
+        for(int index=0;index<maps.size();index++){
+            Map map=maps.get(index);
+            Map map2=mapManager.generateMapForMinutesAgo(Constant.MINUTESDELAYOFMAPAPI+(Constant.SLIDERMAP_MINUTESINTERVAL*index));
+            Assert.assertTrue(map.equals(map2));
+        }
+    }
+
+
 }
--- app/src/main/java/com/quipper/exam/test/domain/Map.java
+++ app/src/main/java/com/quipper/exam/test/domain/Map.java
@@ -11,4 +11,15 @@ public class Map implements Serializable {
     public String description;
     public Date date;
 
+
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof Map){
+            Map map=(Map) o;
+            if(map.imageUrl.equals(imageUrl) && map.date.equals(date) && map.description.equals(description)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
